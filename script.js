5519e16f60953290e90971bc6e4046a6f0362034
srimoorthy <srimoorthy27@gmail.com>
2024-10-29 23:22:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // complete the given function
 
-function palindrome(str){
-
-	let n = str.toLowerCase().split(' ').join(' ')
-	let N = n.split(' ').reverse().join(' ')
-	return n == N
-
+function palindrome(str) {
+    // Convert to lowercase, remove non-alphanumeric characters, and join without spaces
+    let n = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    // Reverse the processed string
+    let N = n.split('').reverse().join('');
+    // Check if the original processed string matches the reversed one
+    return n === N;
 }
-module.exports = palindrome
+
+module.exports = palindrome;
